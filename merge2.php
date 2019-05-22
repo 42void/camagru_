0b34302221b19ced46c27c0baf3bb2d8070dd6d7
Anne-Lise VANHOEGAERDEN <al.vanhoegaerden@gmail.com>
2019-05-22 19:24:44 +0200

    fix cat to big on uploaded images

--- merge2.php
+++ merge2.php
@@ -16,21 +16,22 @@ imagealphablending($src, false);
 imagesavealpha($src, true);
 
 $src_width = imagesx($src);
+// echo "<script>console.log('src_width " .$src_width. "' );</script>";
 $src_height = imagesy($src);
 $dest_width = imagesx($dest);
 $dest_height = imagesy($dest);
 
+$percent = 0.6;
+
 $dest_x = $dest_width - $src_width;
 $dest_y = $dest_height - $src_height;
-
-
 
 if ($dest_width - 235 > 0 && $dest_height - 200 > 0){
   $dest_width = $dest_width-235;
   $dest_height = $dest_height-200;
 }
 
-imagecopyresized($dest, $src, 10, 10, 0, 0, $dest_width , $dest_height , $src_width, $src_height);
+imagecopyresized($dest, $src, 10, 10, 0, 0, $dest_width* $percent , $dest_height* $percent, $src_width, $src_height);
 
 $ret = "./results/".uniqid().".png";
 imagepng($dest, $ret);
