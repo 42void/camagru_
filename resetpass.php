70bd53296b27d9acdb6a6918cabb91f575381cd9
Anne-Lise VANHOEGAERDEN <al.vanhoegaerden@gmail.com>
2019-05-31 16:57:38 +0200

    fix white screen bug resetpass

--- resetpass.php
+++ resetpass.php
@@ -7,9 +7,8 @@ if (empty($_GET['code'])) {
 }
 
 if (isset($_GET['code'])) {
-  $code = $_GET['code'];
   $stmt = $user->runQuery("SELECT * FROM tbl_users WHERE tokenCode=:token");
-  $stmt->bindparam(":token", $code);
+  $stmt->bindparam(":token", $_GET['code']);
   $stmt->execute();
   $row = $stmt->fetch(PDO::FETCH_ASSOC);
   if ($row) {
@@ -18,31 +17,31 @@ if (isset($_GET['code'])) {
       $cpass = $_POST['confirm-pass'];
       if (strlen($pass) < 6) {
         $msg = "<div class='alert alert-block'>
-       <strong>Sorry!</strong>  Yous password must be at least 6 characters long!
+        Yous password must be at least 6 characters long!
        </div>";
       } else if (strlen($pass) > 20) {
         $msg = "<div class='alert alert-block'>
-       <strong>Sorry!</strong>  Your password must be less than 20 characters long!
+        Your password must be less than 20 characters long!
        </div>";
       } else if (!preg_match("#[0-9]+#", $pass)) {
         $msg = "<div class='alert alert-block'>
-       <strong>Sorry!</strong>  Password must include at least one number!
+        Password must include at least one number!
        </div>";
       } else if (!preg_match("#[a-z]+#", $pass)) {
         $msg = "<div class='alert alert-block'>
-       <strong>Sorry!</strong>  Password must include at least one letter!
+        Password must include at least one letter!
        </div>";
       } else if (!preg_match("#[A-Z]+#", $pass)) {
         $msg = "<div class='alert alert-block'>
-       <strong>Sorry!</strong>  Password must include at least one capital letter!
+        Password must include at least one capital letter!
        </div>";
       } else if (!preg_match("#\W+#", $pass)) {
         $msg = "<div class='alert alert-block'>
-       <strong>Sorry!</strong>  Password must include at least one symbol!
+        Password must include at least one symbol!
        </div>";
       } else if ($cpass !== $pass) {
         $msg = "<div class='alert alert-block'>
-        <strong>Sorry!</strong>  Password Doesn't match.
+        Password Doesn't match.
         </div>";
       } else {
         $stmt = $user->runQuery("UPDATE tbl_users SET userPass=:upass WHERE userID=:uid");
@@ -57,7 +56,7 @@ if (isset($_GET['code'])) {
       }
     }
   } else {
-    exit;
+    'Token has expired';
   }
 }
 
@@ -66,7 +65,7 @@ if (isset($_GET['code'])) {
 <html>
 
 <head>
-  <title>Password Reset</title>
+  <title>Password reset</title>
   <link rel="stylesheet" href="index.css" type="text/css">
   <link rel="stylesheet" href="home.css" type="text/css">
 </head>
@@ -76,22 +75,20 @@ if (isset($_GET['code'])) {
 </nav>
 
 <body class="logoutDisplay" id="login">
+  <h3 class="title">Password reset</h3>
   <div>
-    <div>
-      <strong>Hello !</strong> <?php echo $rows['userName'] ?> You're here to reset your forgotten password
-    </div>
     <form method="post">
-      <h3>Password Reset.</h3>
-      <hr />
       <?php
       if (isset($msg)) {
         echo $msg;
       }
       ?>
-      <input class="input" type="password" placeholder="New Password" name="pass" required />
-      <input class="input" type="password" placeholder="Confirm New Password" name="confirm-pass" required />
-      <hr />
-      <button type="submit" class="generateNewPaswdButton" name="btn-reset-pass"><span class="loginButtonText">Reset your Password</span></button>
+      <div class="resetPwInputContainer">
+        <input class="input" type="password" placeholder="New Password" name="pass" required />
+        <input class="input" type="password" placeholder="Confirm New Password" name="confirm-pass" required />
+        <p>Password must be at least be 6 characters long, with 1 number, 1 symbol and 1 capital letter</p>
+        <button type="submit" class="btn" name="btn-reset-pass"><span class="loginButtonText">Reset your Password</span></button>
+      </div>
 
     </form>
 
