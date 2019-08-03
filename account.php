687f4060c2500f8068bec86df29ee14a6d226765
Anne-Lise VANHOEGAERDEN <al.vanhoegaerden@gmail.com>
2019-08-03 17:29:55 +0200

    fix bug : possible to use an already existant username or email

--- account.php
+++ account.php
@@ -10,22 +10,52 @@ if (!$user->is_logged_in()) {
 $stmt = $user->runQuery("SELECT userName, userEmail FROM tbl_users WHERE userID = :session");
 $stmt->bindparam(":session", $_SESSION['userID']);
 $select = $stmt->execute();
-$row=$stmt->fetch(PDO::FETCH_ASSOC);
-$phusername=htmlspecialchars($row['userName']);
-$phemail=htmlspecialchars($row['userEmail']);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$phusername = htmlspecialchars($row['userName']);
+$phemail = htmlspecialchars($row['userEmail']);
+$error_message = false;
+$success_message = false;
 
 if (isset($_POST['btn-update'])) {
   $uname = trim($_POST['txtuname']);
   $email = trim($_POST['txtemail']);
-  $stmt2 = $user->runQuery("UPDATE tbl_users SET userName = :uname, userEmail = :email WHERE userID = :session");
-  $stmt2->bindparam(":uname", $uname);
-  $clean_email = trim(strtolower($email));
-  $stmt2->bindparam(":email", $clean_email);
-  $stmt2->bindparam(":session", $_SESSION['userID']);
-  $update = $stmt2->execute();
-  if ($update) {
-    $_SESSION['success'] = 1;
-    Header('Location: '.htmlspecialchars($_SERVER['PHP_SELF']));
+
+  if($phusername == $uname && $phemail == $email) {
+    $error_message = "Error: trying to change to the same username and email";
+  } else {
+    $stmt2 = $user->runQuery("SELECT * FROM tbl_users WHERE userName = :uname AND userName != :usession");
+    $stmt2->bindparam(":uname", $uname);
+    $stmt2->bindparam(":usession", $phusername);
+    $result2 = $stmt2->execute();
+
+    $stmt4 = $user->runQuery("SELECT * FROM tbl_users WHERE userEmail = :email AND userName != :usession");
+    $stmt4->bindparam(":email", $email);
+    $stmt4->bindparam(":usession", $phusername);
+    $result4 = $stmt4->execute();
+
+    if(!$result2 || !$result4) {
+      $error_message = "Data base error: could not check if user or email exist already.";
+    } else {
+      $countusernames = count($stmt2->fetchAll());
+      $countemails = count($stmt4->fetchAll());
+      if($countusernames > 0) {
+        $error_message = "Error: username already exists.";
+      } else if($countemails > 0) {
+        $error_message = "Error: email already exists.";
+      } else {
+        $stmt3 = $user->runQuery("UPDATE tbl_users SET userName = :uname, userEmail = :email WHERE userID = :session");
+        $stmt3->bindparam(":uname", $uname);
+        $clean_email = trim(strtolower($email));
+        $stmt3->bindparam(":email", $clean_email);
+        $stmt3->bindparam(":session", $_SESSION['userID']);
+        $update = $stmt3->execute();
+        if ($update) {
+          $success_message = "Your credentials have been updated";
+          $phusername = $uname;
+          $phemail = $email;
+        }
+      }
+    }
   }
 }
 
@@ -46,13 +76,21 @@ if (isset($_POST['btn-update'])) {
     </nav>
     <h1 class="title">Account settings</h1>
 
-    <form class="form" action="account.php" method="post">
-      <input class="input" id="username" type="text" value=<?php if(isset($phusername)) echo $phusername; else echo "username"; ?> name="txtuname" required />
-      <input class="input" id="email" type="email" value=<?php if(isset($phemail)) echo $phemail; else echo "email"; ?> name="txtemail" required />
+    <form class="form" method="post">
+      <input class="input" id="username" type="text" value=<?php if (isset($phusername)) echo $phusername;
+                                                            else echo "username"; ?> name="txtuname" required />
+      <input class="input" id="email" type="email" value=<?php if (isset($phemail)) echo $phemail;
+                                                          else echo "email"; ?> name="txtemail" required />
       <div>
         <div id='msg'></div>
         <div class="signup-btn-container">
-          <?php if(isset($_SESSION['success'])){echo "Your credentials have been updated";}?>
+        <?php if ($success_message !== false) {
+            echo $success_message;
+          } ?>
+          <?php if ($error_message !== false) {
+            echo $error_message;
+          } ?>
+
           <button type="submit" class="btn" name="btn-update">
             Update
           </button>
